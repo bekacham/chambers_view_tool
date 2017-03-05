7efdda9f8c07052a1977ad707c6b4ed3f34817e3
Rebekah Chambers <bekachambers@Rebekahs-MacBook-Air.local>
2017-03-05 08:39:21 -0700

    dev tool initial commit

---
+++ lib/chambers_view_tool.rb
@@ -0,0 +1,6 @@
+require "chambers_view_tool/version"
+require "chambers_view_tool/renderer"
+
+module ChambersViewTool
+  # Your code goes here...
+end
---
+++ lib/chambers_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ChambersViewTool
+  VERSION = "0.1.0"
+end
